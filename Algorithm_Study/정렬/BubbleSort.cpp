a6b66e92432134cec1ced926939af02fef1ed003
HD152513 <qag331@naver.com>
2021-01-08 22:52:43 +0900

    삽입정렬, 버블정렬 일부 수정

--- Algorithm_Study/정렬/BubbleSort.cpp
+++ Algorithm_Study/정렬/BubbleSort.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 //Bubble Sort
 int main(){
+	int i, j, temp;
 	int arr[10] = {8, 1, 2, 4, 9, 3, 6, 5, 10, 7};
-	int temp;
-	for(int i = 0; i < 10; i++){
-	 	for(int j = 0; j < 9-i; j++){
+	for(i = 0; i < 10; i++){
+	 	for(j = 0; j < 9-i; j++){
 	 		if(arr[j] > arr[j+1]){
 	 			temp = arr[j];
 	 			arr[j] = arr[j+1];
@@ -13,7 +13,7 @@ int main(){
 			 }
 		 }
 	}
-	for(int i = 0; i < 10; i++){
+	for(i = 0; i < 10; i++){
 		cout<<arr[i]<<" ";
 	}
 	return 0;
--- Algorithm_Study/정렬/InsertionSort.cpp
+++ Algorithm_Study/정렬/InsertionSort.cpp
@@ -2,18 +2,16 @@
 using namespace std;
 //Insertion Sort
 int main(){
+	int i, j, temp;
 	int arr[10] = {8, 1, 2, 4, 9, 3, 6, 5, 10, 7};
-	int i, j, key;
 	for(i = 1; i < 10; i++){
-		key = arr[i];
-		for(j = i - 1; j >= 0; j--){
-			if(arr[j] > key){
-				arr[j+1] = arr[j];
-			}else{
-				break;
-			}
+		j = i;
+		while(j > 0 && arr[j-1] > arr[j]){
+			temp = arr[j-1];
+			arr[j-1] = arr[j];
+			arr[j] = temp;
+			j--;
 		}
-		arr[j+1] = key;
 	}
 	for(int i = 0; i < 10; i++){
 		cout<<arr[i]<<" ";
