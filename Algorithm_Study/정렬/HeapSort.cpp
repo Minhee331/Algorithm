15e51561245cb90e561ebdb271e34a4f0645d5a3
HD152513 <qag331@naver.com>
2021-01-09 22:25:36 +0900

    210109 주석 추가

--- Algorithm_Study/정렬/HeapSort.cpp
+++ Algorithm_Study/정렬/HeapSort.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
 	int i, j, temp, c, root;
 	int heap[10] = {8, 1, 2, 4, 9, 3, 6, 5, 10, 7};
-	for(i=1; i<10; i++){
+	for(i=1; i<10; i++){ //힙을 구성한다.  
 		c = i;
 		do{
 			int root = (c-1)/2;
@@ -15,6 +15,7 @@ int main(){
 			c = root;
 		}while(c != 0);
 	}
+	// 반복적으로 힙을 구성한다.  
 	for(i = 9; i >= 0; i--){
 		temp = heap[0];
 		heap[0] = heap[i];
@@ -23,10 +24,10 @@ int main(){
 		c = 1;
 		do{
 			c = 2 * root + 1;
-			if(c < i-1 && heap[c] < heap[c+1]){
+			if(c < i-1 && heap[c] < heap[c+1]){ //자식 중 더 큰 값을 찾는다.  
 				c++;
 			}
-			if(c < i && heap[root] < heap[c]){
+			if(c < i && heap[root] < heap[c]){ //루트보다 자식이 크다면 swap 
 				temp = heap[root];
 				heap[root] = heap[c];
 				heap[c] = temp;
--- Algorithm_Study/정렬/QuickSort.cpp
+++ Algorithm_Study/정렬/QuickSort.cpp
@@ -1,38 +1,40 @@
 #include<iostream>
 using namespace std;
+
 void quickSort(int arr[], int start, int end){
-	if(start >= end){
+	
+	if(start >= end){ //원소가 1개인 경우 리턴 
 		return;
 	}
-	int key = start;
-	int i = start+1;
-	int j = end;
+	int pivot = start; //pivot은 start위치의 원소로 둔다. 
+	int left = start+1;
+	int right = end;
 	int temp;
-	while(i<=j){
-		while(i<=end && arr[i] <= arr[key]){
-			i++;
+	while(left<=right){ //left와 right가 교차할 떄까지 반복한다. 
+		while(left<=end && arr[left] <= arr[pivot]){ //pivot보다 큰 값을 만날때까지 left++ 
+			left++;
 		}
-		while(j>start && arr[j] >= arr[key]){
-			j--;
+		while(right>start && arr[right] >= arr[pivot]){ //pivot보다 작은 값을 만날때까지 right-- 
+			right--;
 		}
-		if(i>j){
-			temp = arr[j];
-			arr[j] = arr[key];
-			arr[key] = temp;
-		}else{
-			temp = arr[i];
-			arr[i] = arr[j];
-			arr[j] = temp;
+		if(left>right){ //left와 right가 교차된 상태면 pivot값과 right값을 swap 
+			temp = arr[right];
+			arr[right] = arr[pivot];
+			arr[pivot] = temp;
+		}else{ //교차되지 않았다면 left와 right 값을 swap 
+			temp = arr[left];
+			arr[left] = arr[right];
+			arr[right] = temp;
 		}
-		quickSort(arr, start, j-1);
-		quickSort(arr, j+1, end);
+		quickSort(arr, start, right-1); //앞부분 정렬
+		quickSort(arr, right+1, end); //뒷부분 정렬  
 	}
 }
 int main(){
-	int arr[10] = {8, 1, 2, 4, 9, 3, 6, 5, 10, 7};
-	quickSort(arr, 0, 9);
+	int arr[10] = {8, 1, 2, 4, 9, 3, 6, 5, 10, 7}; //정렬할 배열 
+	quickSort(arr, 0, 9); //quickSort함수 호출(0 ~ n-1 범위) 
 	for (int i = 0; i < sizeof(arr) / sizeof(int); i++) {
-        printf("%d ", arr[i]);
+        printf("%d ", arr[i]); //정렬된 배열을 출력 
     }
     return 0;
 }
