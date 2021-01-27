90551518219f18bded5c2531037de15cbdd68457
HD152513 <qag331@naver.com>
2021-01-28 00:27:28 +0900

    210127| 백준| 2178번

--- BaekJoon/그래프/2178.cpp
+++ BaekJoon/그래프/2178.cpp
@@ -3,26 +3,28 @@
 #include <queue>
 using namespace std;
 
-int maze[100][100];
-int visited[100][100] = {0,};
-int n, m;
+int maze[100][100]; //미로 정보 
+int visited[100][100] = {0,}; //깊이 역할 
+int n, m; //행, 열 
 
 void BFS(int x, int y){
-	int xx[4] = {0, 1, 0, -1}; // 아래, 우측, 위, 좌측 순으로 탐색 시도 
-	int yy[4] = {-1, 0, 1, 0};
+	int xx[4] = {0, -1, 0, 1}; // 상하좌우 탐색 시도 
+	int yy[4] = {1, 0, -1, 0};
 	queue<pair<int, int> > q; //queue 선언
-	q.push(make_pair(x, y)); //queue에 시작점을 넣는다.
-	visited[x][y]++; 
+	q.push(make_pair(y, x)); //queue에 시작점을 넣는다.
+	visited[y][x] = 1;
+	
 	while(!q.empty()){ //q가 empty가 아닐때까지 반복 
-		int currentX = q.front().first; //q의 맨 앞부분 x을 current에 저장
-		int currentY = q.front().second; //q의 맨 앞부분y을 current에 저장 
+		int currentY = q.front().first; //q의 맨 앞부분 Y을 current에 저장
+		int currentX = q.front().second; //q의 맨 앞부분X을 current에 저장 
 		q.pop(); //pop 
+		
 		for(int i=0; i<4; i++){
-			int nextX =  x + xx[i]; //좌표 업데이트 
-			int nextY = y + yy[i]; //좌표 업데이트  
-			if( (nextX>0 && nextX< n) && (nextY>0 && nextY<m)){ //만약 업데이트된 좌표가 존재하는 좌표라면,  
-				q.push(make_pair(nextX, nextY)); //q에 다음 좌표를 넣어준다.  
-				visited[nextX][nextY] = visited[currentX][currentY] + 1; //방문 처리 
+			int nextY = currentY + yy[i]; //좌표 업데이트  
+			int nextX =  currentX + xx[i]; //좌표 업데이트 
+			if( (nextX>=0 && nextX< m) && (nextY>=0 && nextY<n) && maze[nextY][nextX]==1 &&visited[nextY][nextX]==0){ //만약 업데이트된 좌표가 존재하는 좌표이고, 미로좌표가 1(방문 가능),방문하지 않았던  좌표이면,  
+				q.push(make_pair(nextY, nextX)); //q에 다음 좌표를 넣어준다.  
+				visited[nextY][nextX] = visited[currentY][currentX] + 1; //방문 처리 
 			} 
 		}
 	}
@@ -36,5 +38,5 @@ int main(){
 		}
 	}
 	BFS(0, 0);
-	printf("%d\n", visited[n-1][m-1]+1);
+	printf("%d\n", visited[n-1][m-1]);
 }
