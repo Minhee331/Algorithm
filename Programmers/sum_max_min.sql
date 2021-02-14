43c525ad7119a5f4f8b7b22ad675418f3fcb841b
HD152513 <qag331@naver.com>
2021-02-14 21:22:31 +0900

    210214| SQL| GROUP BY

--- Programmers/sum_max_min.sql
+++ Programmers/sum_max_min.sql
@@ -1,11 +1,16 @@
+-- 프로그래머스 SQL 고득점 Kit
 --최댓값 구하기
-SELECT MAX(DATETIME) from ANIMAL_INS
+SELECT MAX(DATETIME) 
+FROM ANIMAL_INS
 
 --최솟값 구하기
-SELECT MIN(DATETIME) from ANIMAL_INS
+SELECT MIN(DATETIME) 
+FROM ANIMAL_INS
 
 --동물 수 구하기
-SELECT COUNT(*) from ANIMAL_INS
+SELECT COUNT(*) 
+FROM ANIMAL_INS
 
 --중복 제거하기
-SELECT count(distinct name) from ANIMAL_INS
+SELECT count(distinct name) 
+FROM ANIMAL_INS
